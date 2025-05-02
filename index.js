7354118402891ab7f3c045129213c18a5724bec2
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2025-05-02 23:35:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process')
 const { existsSync, writeFileSync } = require('fs')
 
-const SESSION_ID = 'levanter_1b6bcb2aef13f45d586afac9091ef9051' // Edit this line only, don't remove ' <- this symbol
+const SESSION_ID = 'levanter_303f4ebe2bc3447fe990d9f3a706142d5' // Edit this line only, don't remove ' <- this symbol
 
 if (!existsSync('levanter')) {
   console.log('Cloning the repository...')
